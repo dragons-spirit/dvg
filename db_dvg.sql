41d239126605f874cb7bbf3eecb2dda170fe6836
Feuerdrache <lone.ranger@gmx.de>
2016-06-02 00:17:49 +0200

    Neue Datenbank
    * Neue Datenbankstruktur inklusive Übersicht
    * Ordner mit nützlichen Links

---
+++ db_dvg.sql
@@ -0,0 +1,420 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 02. Jun 2016 um 00:05
+-- Server-Version: 10.1.13-MariaDB
+-- PHP-Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `db_dvg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(10) NOT NULL,
+  `login` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `passwort` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `aktiv` tinyint(1) NOT NULL,
+  `letzter_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bilder`
+--
+
+CREATE TABLE `bilder` (
+  `id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `pfad` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `element`
+--
+
+CREATE TABLE `element` (
+  `id` int(10) NOT NULL,
+  `element_id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `faehigkeiten`
+--
+
+CREATE TABLE `faehigkeiten` (
+  `id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `faehigkeiten_spieler`
+--
+
+CREATE TABLE `faehigkeiten_spieler` (
+  `id` int(10) NOT NULL,
+  `spieler_id` int(10) NOT NULL,
+  `faehigkeiten_id` int(10) NOT NULL,
+  `wert` float NOT NULL,
+  `stufe` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `gattung`
+--
+
+CREATE TABLE `gattung` (
+  `id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `start_staerke` float NOT NULL,
+  `start_intelligenz` float NOT NULL,
+  `start_magie` float NOT NULL,
+  `start_element_feuer` float NOT NULL,
+  `start_element_wasser` float NOT NULL,
+  `start_element_erde` float NOT NULL,
+  `start_element_luft` float NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `level`
+--
+
+CREATE TABLE `level` (
+  `id` int(10) NOT NULL,
+  `voraussetzung` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `stufe` int(10) NOT NULL,
+  `modifikator` float NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `npc`
+--
+
+CREATE TABLE `npc` (
+  `id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `gattung_id` int(10) NOT NULL,
+  `element_id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `titel_erweitert` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `staerke` float NOT NULL,
+  `intelligenz` float NOT NULL,
+  `magie` float NOT NULL,
+  `element_feuer` float NOT NULL,
+  `element_wasser` float NOT NULL,
+  `element_erde` float NOT NULL,
+  `element_luft` float NOT NULL,
+  `gesundheit` int(10) NOT NULL,
+  `energie` int(10) NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `position`
+--
+
+CREATE TABLE `position` (
+  `id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `quest`
+--
+
+CREATE TABLE `quest` (
+  `id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `titel_erweitert` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `voraussetzung_level` int(10) NOT NULL,
+  `belohnung` int(10) NOT NULL,
+  `text_start` text COLLATE utf8_unicode_ci NOT NULL,
+  `text_mitte` text COLLATE utf8_unicode_ci NOT NULL,
+  `text_sieg` text COLLATE utf8_unicode_ci NOT NULL,
+  `text_niederlage` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `quest_spieler`
+--
+
+CREATE TABLE `quest_spieler` (
+  `id` int(10) NOT NULL,
+  `spieler_id` int(10) NOT NULL,
+  `quest_id` int(10) NOT NULL,
+  `start` timestamp NULL DEFAULT NULL,
+  `ende` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `spieler`
+--
+
+CREATE TABLE `spieler` (
+  `id` int(10) NOT NULL,
+  `account_id` int(10) NOT NULL,
+  `bilder_id` int(10) NOT NULL,
+  `gattung_id` int(10) NOT NULL,
+  `level_id` int(10) NOT NULL,
+  `name` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
+  `straerke` float NOT NULL COMMENT 'Stärke',
+  `intelligenz` float NOT NULL COMMENT 'Intelligenz',
+  `magie` float NOT NULL COMMENT 'Magie',
+  `element_feuer` float NOT NULL,
+  `element_wasser` float NOT NULL,
+  `element_erde` float NOT NULL,
+  `element_luft` float NOT NULL,
+  `gesundheit` int(10) NOT NULL,
+  `energie` int(10) NOT NULL,
+  `balance` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `zauber`
+--
+
+CREATE TABLE `zauber` (
+  `id` int(10) NOT NULL,
+  `spieler_id` int(10) NOT NULL,
+  `element_id` int(10) NOT NULL COMMENT 'Zuordnung zu Element',
+  `bilder_id` int(10) NOT NULL,
+  `zauberart_id` int(10) NOT NULL COMMENT 'aktiv/passiv, defensiv/offensiv',
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `titel_erweitert` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `voraussetzung` int(10) NOT NULL,
+  `verbrauch` int(10) NOT NULL,
+  `effekt` float NOT NULL COMMENT 'Grundwert für Angriff/Verteidigung und sonstige Effekte',
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `zauberart`
+--
+
+CREATE TABLE `zauberart` (
+  `id` int(10) NOT NULL,
+  `titel` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `modifikator` float NOT NULL,
+  `beschreibung` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `bilder`
+--
+ALTER TABLE `bilder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `element`
+--
+ALTER TABLE `element`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `faehigkeiten`
+--
+ALTER TABLE `faehigkeiten`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `faehigkeiten_spieler`
+--
+ALTER TABLE `faehigkeiten_spieler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `gattung`
+--
+ALTER TABLE `gattung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `npc`
+--
+ALTER TABLE `npc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `quest`
+--
+ALTER TABLE `quest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `quest_spieler`
+--
+ALTER TABLE `quest_spieler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `spieler`
+--
+ALTER TABLE `spieler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `zauber`
+--
+ALTER TABLE `zauber`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `zauberart`
+--
+ALTER TABLE `zauberart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `bilder`
+--
+ALTER TABLE `bilder`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `element`
+--
+ALTER TABLE `element`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `faehigkeiten`
+--
+ALTER TABLE `faehigkeiten`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `faehigkeiten_spieler`
+--
+ALTER TABLE `faehigkeiten_spieler`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `gattung`
+--
+ALTER TABLE `gattung`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `level`
+--
+ALTER TABLE `level`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `npc`
+--
+ALTER TABLE `npc`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `position`
+--
+ALTER TABLE `position`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `quest`
+--
+ALTER TABLE `quest`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `quest_spieler`
+--
+ALTER TABLE `quest_spieler`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `spieler`
+--
+ALTER TABLE `spieler`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `zauber`
+--
+ALTER TABLE `zauber`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `zauberart`
+--
+ALTER TABLE `zauberart`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
