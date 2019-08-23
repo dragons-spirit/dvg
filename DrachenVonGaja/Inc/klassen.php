47fde8acac68065721f650369c061e1365277383
Feuerdrache-mobil <lone.ranger@gmx.de>
2019-08-23 19:25:34 +0200

    Neue Klasse noch optimiert

--- DrachenVonGaja/Inc/klassen.php
+++ DrachenVonGaja/Inc/klassen.php
@@ -11,14 +11,7 @@ class LoginSpieler {
 	public $geschlecht;
 
 	public function __construct($ds=null) {
-		$this->id = $ds[0];
-		$this->account_id = $ds[1];
-		$this->bilder_id = $ds[2];
-		$this->gattung = $ds[3];
-		$this->level_id = $ds[4];
-		$this->startgebiet = $ds[5];
-		$this->name = $ds[6];
-		$this->geschlecht = $ds[7];
+		$this->set($ds);
 	}
 	
 	public function set($ds) {
