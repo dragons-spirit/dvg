bfcb95c9b2642b5eb28e0c01630bb818afd2cd6e
Feuerdrache <lone.ranger@gmx.de>
2020-08-02 17:39:23 +0200

    Story Korrektur gelesen

--- DrachenVonGaja/Inc/story.php
+++ DrachenVonGaja/Inc/story.php
@@ -44,12 +44,12 @@ a:active
 
 <h1 align="center">Vom Sternendrachen Sangha</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
-"Einst existierte der Sternendrache <b>Sangha</b> als das reinste und größte Seelenwesen und kosmischen Bewussteins.<br>
-Das Wort Sangha bedeutet in Sanskrit Versammlung und es beschreibt das eine Bewusstsein, dass sich aus allen Bewusstseinen <br>
-der ganzen Welt Gajas zusammensetzt. Bevor sich die Welt Gaja manifestierte, gab es vorher das kosmische Bewusstsein Sangha <br>
-und es war ein Drachenbewusstsein gewesen, denn seine innere Energie der Liebe kann manchmal so heiß und zerstörerisch sein <br>
-wie die Flammen eines wütenden Drachens. Doch bevor diese Energie auf der Welt Gaja Schaden anrichtete, erschuff es das Sternenall<br>
-als Sanghas Seele und auch alle anderen Seelen."<br></p>
+"Einst existierte der Sternendrache <b>Sangha</b> als das reinste und größte Seelenwesen und kosmischen Bewussteins.<br />
+Das Wort Sangha bedeutet im Sanskrit Versammlung und es beschreibt das eine Bewusstsein, dass sich aus allen Bewusstseinen <br />
+der ganzen Welt Gajas zusammensetzt. Bevor sich die Welt Gaja manifestierte, gab es vorher das kosmische Bewusstsein Sangha <br />
+und es war ein Drachenbewusstsein gewesen, denn seine innere Energie der Liebe kann manchmal so heiß und zerstörerisch sein <br />
+wie die Flammen eines wütenden Drachens. Doch bevor diese Energie auf der Welt Gaja Schaden anrichtete, erschuff es das Sternenall<br />
+als Sanghas Seele und auch alle anderen Seelen."<br /></p>
 
 <h1 align="center">Was ist die Welt Gaja ?</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
@@ -58,11 +58,11 @@ Auf dieser Welt manifestierte Sangha mit seiner Fantasie alle möglichen Pflanze
 
 <h1 align="center">Wer sind die Drachen von Gaja ?</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
-"Die Drachen von Gaja sind die Bewohner des Planetens Gaja. Früher waren sie Menschen gewesen bis sie in einem neuen Leben<br>
-als kleine Drachen aus Eiern geschlüpft waren. Als Drache bekamen die Menschen die Chance die Welt aus einen anderen Blickwinkel<br>
-kennen zu lernen und die Elemente zu beherrschen die ihre Welt einst erschuff und formte."<br>
-Wenn du auch ein neues Leben als Drache anfangen möchtest, dann melde dich bei uns an !</p>
+"Die Drachen von Gaja sind die Bewohner des Planeten Gaja. Früher waren sie Menschen gewesen bis sie in einem neuen Leben<br />
+als kleine Drachen aus Eiern geschlüpft waren. Als Drachen bekamen die Menschen die Chance die Welt aus einem anderen Blickwinkel<br />
+kennen zu lernen und die Elemente zu beherrschen, die ihre Welt einst erschuf und formte."<br /><br />
 
+Wenn du auch ein neues Leben als Drache anfangen möchtest, dann melde dich bei uns an !</p>
 
 <p align="center"><a class="storylink" href="../index.php" alt="zurueck"/>Zurück</a></p>
 
