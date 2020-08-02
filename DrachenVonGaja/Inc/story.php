825b1b1cef557a46f95456813b2bf6fc600ddfb8
Eisdrache <lone.ranger@gmx.de>
2020-08-02 17:17:57 +0200

    Update story.php
    Storyupdate

--- DrachenVonGaja/Inc/story.php
+++ DrachenVonGaja/Inc/story.php
@@ -29,27 +29,26 @@ a:active
                                 
 </style>
 
-<h1 align="center">Geschichte der Drachen von Gaja</h1>
+<h1 align="center">Vom Sternendrachen Sangha</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
-"Einst existierte der Lichtdrache <b>Sangha</b> als das reinste und gr&ouml;&szlig;te Seelenwesen.<br>
-Der Lichtdrache manifestierte sich als die gro&szlig;e materielle See aus Lichtern (Weltall)<br>
-in der au&szlig;erirdischen Welt und unendlich - fach, sowie paarweise (als m&auml;nnlich und weiblich)<br>
-als winzige materielle Lebewesen auf der irdischen Welt. <br>
-Alle Lichter der gro&szlig;en materiellen See sind seine Ab - und Spiegelbilder sowie auch alle Lebewesen seine Ab- und Spiegelbilder sind.<br>
-Trotz der manigfaltigen Formen von Leben in die sich der Lichtdrache inkarnierte, beschloss er<br>
-sich als ein Wesen zu manifestieren, dass die F&auml;higkeit zur Erinnerung besa&szlig;, falls er sein<br>
-wahres Selbst je vergessen w&uuml;rde.<br>
-So inkarnierte er sich als Dracheng&ouml;ttin die die Erde Gaja erschuff<br>
-und als Drachengott der den Himmel &uuml;ber Gaja erschuff.<br>
-Nach der Erschaffung von Erde Gaja und dem Himmel w&uuml;nschten sich beide Dracheng&ouml;tter<br>
-die erschaffende Erde Gaja betreten und den erschaffenen Himmel befliegen zu k&ouml;nnen und so<br>
-materialisierten sie sich in die Welt. <br>
-Nachdem die zwei G&ouml;tter auf Gaja erschienen und sich materialisiert hatten,<br>
-waren sie so stark ineinander verliebt, dass sie verga&szlig;en, dass sie Dracheng&ouml;tter waren und einst <br>
-als der Lichtdrache Sangha die ganze Welt geschaffen hatten. <br>
-Fortan waren sie das erste Drachenpaar auf Gaja welches die Welt von Gaja bev&ouml;lkerte bis..."<br><p>
-<p>An dieser Stelle rei&szlig;t die Geschichte ab. <br>
-Wenn du mehr &uuml;ber die Geschichte und die Geheimnisse von Gaja erfahren m&ouml;chtest, melde dich bei uns an !</p>
+"Einst existierte der Sternendrache <b>Sangha</b> als das reinste und gr&ouml;&szlig;te Seelenwesen und kosmischen Bewussteins.<br>
+Das Wort „Sangha“ bedeutet in Sanskrit „Versammlung“ und es beschreibt das eine Bewusstsein, dass sich aus allen Bewusstseinen <br>
+der ganzen Welt Gajas zusammensetzt. Bevor sich die Welt Gaja manifestierte, gab es vorher das kosmische Bewusstsein Sangha <br>
+und es war ein Drachenbewusstsein gewesen, denn seine innere Energie der Liebe kann manchmal so heiß und zerstörerisch sein <br>
+wie die Flammen eines wütenden Drachens. Doch bevor diese Energie auf der Welt Gaja Schaden anrichtete, erschuff es das Sternenall<br>
+als Sanghas Seele und auch alle anderen Seelen."<br></p>
+
+<h1 align="center">Was ist die Welt Gaja ?</h1>
+<p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
+"Die Welt Gaja stellt den Ort der höchsten Verdichtung in Sanghas Sternenleib dar und liegt in seinem Schoß verborgen.
+Auf dieser Welt manifestierte Sangha mit seiner Fantasie alle möglichen Pflanzen, Tiere und Steine."</p>
+
+<h1 align="center">Wer sind die Drachen von Gaja ?</h1>
+<p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
+"Die Drachen von Gaja sind die Bewohner des Planetens Gaja. Früher waren sie Menschen gewesen bis sie in einem neuen Leben<br>
+als kleine Drachen aus Eiern geschlüpft waren. Als Drache bekamen die Menschen die Chance die Welt aus einen anderen Blickwinkel<br>
+kennen zu lernen und die Elemente zu beherrschen die ihre Welt einst erschuff und formte."</b>
+Wenn du auch ein neues Leben als Drache anfängen möchtest, dann melde dich bei uns an !</p>
 
 
 <p align="center"><a class="storylink" href="../index.php" alt="zurueck"/>Zur&uuml;ck</a></p>
