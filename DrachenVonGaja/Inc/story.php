fc88b46bd121dc319b982b0c7d98e498e7b56431
Feuerdrache <lone.ranger@gmx.de>
2020-08-02 17:32:17 +0200

    Kodierung Story geändert

--- DrachenVonGaja/Inc/story.php
+++ DrachenVonGaja/Inc/story.php
@@ -3,7 +3,10 @@
         <title>
             Wer sind die Drachen von Gaja ?
         </title>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Language" content="de">
+		<meta http-equiv="Content-Script-Type" content="text/javascript">
+		<meta http-equiv="Content-Style-Type" content="text/css">
+		<meta charset="utf-8">
     </head>
 
 <body>
@@ -41,27 +44,27 @@ a:active
 
 <h1 align="center">Vom Sternendrachen Sangha</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
-"Einst existierte der Sternendrache <b>Sangha</b> als das reinste und gr&ouml;&szlig;te Seelenwesen und kosmischen Bewussteins.<br>
-Das Wort „Sangha“ bedeutet in Sanskrit „Versammlung“ und es beschreibt das eine Bewusstsein, dass sich aus allen Bewusstseinen <br>
+"Einst existierte der Sternendrache <b>Sangha</b> als das reinste und gr√∂√üte Seelenwesen und kosmischen Bewussteins.<br>
+Das Wort Sangha bedeutet in Sanskrit Versammlung und es beschreibt das eine Bewusstsein, dass sich aus allen Bewusstseinen <br>
 der ganzen Welt Gajas zusammensetzt. Bevor sich die Welt Gaja manifestierte, gab es vorher das kosmische Bewusstsein Sangha <br>
-und es war ein Drachenbewusstsein gewesen, denn seine innere Energie der Liebe kann manchmal so heiß und zerstörerisch sein <br>
-wie die Flammen eines wütenden Drachens. Doch bevor diese Energie auf der Welt Gaja Schaden anrichtete, erschuff es das Sternenall<br>
+und es war ein Drachenbewusstsein gewesen, denn seine innere Energie der Liebe kann manchmal so hei√ü und zerst√∂rerisch sein <br>
+wie die Flammen eines w√ºtenden Drachens. Doch bevor diese Energie auf der Welt Gaja Schaden anrichtete, erschuff es das Sternenall<br>
 als Sanghas Seele und auch alle anderen Seelen."<br></p>
 
 <h1 align="center">Was ist die Welt Gaja ?</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
-"Die Welt Gaja stellt den Ort der höchsten Verdichtung in Sanghas Sternenleib dar und liegt in seinem Schoß verborgen.
-Auf dieser Welt manifestierte Sangha mit seiner Fantasie alle möglichen Pflanzen, Tiere und Steine."</p>
+"Die Welt Gaja stellt den Ort der h√∂chsten Verdichtung in Sanghas Sternenleib dar und liegt in seinem Scho√ü verborgen.
+Auf dieser Welt manifestierte Sangha mit seiner Fantasie alle m√∂glichen Pflanzen, Tiere und Steine."</p>
 
 <h1 align="center">Wer sind die Drachen von Gaja ?</h1>
 <p align="center" style="font-family:Lucida Calligraphy,Georgia,fantasy,EG Dragon Caps;font-size:18px;">
-"Die Drachen von Gaja sind die Bewohner des Planetens Gaja. Früher waren sie Menschen gewesen bis sie in einem neuen Leben<br>
-als kleine Drachen aus Eiern geschlüpft waren. Als Drache bekamen die Menschen die Chance die Welt aus einen anderen Blickwinkel<br>
+"Die Drachen von Gaja sind die Bewohner des Planetens Gaja. Fr√ºher waren sie Menschen gewesen bis sie in einem neuen Leben<br>
+als kleine Drachen aus Eiern geschl√ºpft waren. Als Drache bekamen die Menschen die Chance die Welt aus einen anderen Blickwinkel<br>
 kennen zu lernen und die Elemente zu beherrschen die ihre Welt einst erschuff und formte."<br>
-Wenn du auch ein neues Leben als Drache anfängen möchtest, dann melde dich bei uns an !</p>
+Wenn du auch ein neues Leben als Drache anfangen m√∂chtest, dann melde dich bei uns an !</p>
 
 
-<p align="center"><a class="storylink" href="../index.php" alt="zurueck"/>Zur&uuml;ck</a></p>
+<p align="center"><a class="storylink" href="../index.php" alt="zurueck"/>Zur√ºck</a></p>
 
 </body>
 </html>
