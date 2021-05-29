605fb63b072f385cc021593fa05f6477106926f8
Eisdrache <lone.ranger@gmx.de>
2021-05-29 10:54:11 +0200

    ein paar änderungen

--- DrachenVonGaja/Inc/story.php
+++ DrachenVonGaja/Inc/story.php
@@ -51,23 +51,23 @@ h1
                                 
 </style>
 
-<h1 align="center">Vom Sternendrachen Sangha</h1>
+<h1 align="center">Vom Sternendrachen Sangha und Gaja</h1>
 <p align="center">
-"Einst existierte der Sternendrache <b>Sangha</b> als das reinste und größte Seelenwesen und kosmischen Bewussteins.<br />
-Das Wort Sangha bedeutet im Sanskrit Versammlung und es beschreibt das eine Bewusstsein, dass sich aus allen Bewusstseinen <br />
-der ganzen Welt Gajas zusammensetzt. Bevor sich die Welt Gaja manifestierte, gab es vorher das kosmische Bewusstsein Sangha <br />
-und es war ein Drachenbewusstsein gewesen, denn seine innere Energie der Liebe kann manchmal so heiß und zerstörerisch sein <br />
-wie die Flammen eines wütenden Drachens. Doch bevor diese Energie auf der Welt Gaja Schaden anrichtete, erschuff es das Sternenall<br />
-als Sanghas Seele und auch alle anderen Seelen."<br /></p>
-
-<h1 align="center">Was ist die Welt Gaja ?</h1>
-<p align="center">
-"Die Welt Gaja stellt den Ort der höchsten Verdichtung in Sanghas Sternenleib dar und liegt in seinem Schoß verborgen.<br />
-Auf dieser Welt manifestierte Sangha mit seiner Fantasie alle möglichen Pflanzen, Tiere und Steine."</p>
+"Einst existierte ein großer Sternendrache namens <b>Sangha</b> als das reinste und größte Seelenwesen und kosmische Bewusstein.<br />
+Sangha war ein Drache, denn seine innere Energie der Liebe kann manchmal so heiß und zerstörerisch sein <br />
+wie die Flammen eines wütenden Drachens. <br />
+Eines Tages hatte Sangha den Wunsch nach einen Körper bekommen. <br />
+Weil Sangha sich selbst erkennen und erfühlen wollte, teilte er seine Seele in zwei Hälften auf. <br />
+Durch diese Teilung erschuff er einen männlichen Drachengott und eine weibliche Drachengöttin. <br />
+Nun konnte Sangha sich selbst erkennen und erfühlen. <br />
+Nach einer Weile bekamen der Drachengott und die Drachengöttin den Wunsch sich vereinen zu wollen. <br />
+Durch diese Vereinigung wurde in der Drachengöttin ein neuer kleiner Drache gezeugt der im weiblichen Drachenschoß heranwuchs, <br />
+ernährt von seiner eigenen Sonne. <br /> Der kleine Drache erhielt den Namen <b>Gaja</b>. <br />
+Auf Gaja manifestierte sich Sanghas Liebe und Schöpfungskraft mit seiner Fantasie als alle möglichen Pflanzen und Tiere."</p>
 
 <h1 align="center">Wer sind die Drachen von Gaja ?</h1>
 <p align="center">
-"Die Drachen von Gaja sind die Bewohner des Planeten Gaja. Früher waren sie Menschen gewesen bis sie in einem neuen Leben<br />
+"Die Drachen von Gaja sind die Bewohner des Drachenkindes Gaja das sie für ihren Planeten halten da Gaja noch eine nahezu kugelige Form hat.<br /> Früher waren sie Menschen gewesen bis sie in einem neuen Leben<br />
 als kleine Drachen aus Eiern geschlüpft waren. Als Drachen bekamen die Menschen die Chance die Welt aus einem anderen Blickwinkel<br />
 kennen zu lernen und die Elemente zu beherrschen, die ihre Welt einst erschuf und formte."<br /><br />
 
